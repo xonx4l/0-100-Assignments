434235a2305dde2c97f65325e5ec7d07ff6657f1
xonx <119700621+xonx4l@users.noreply.github.com>
2023-12-09 17:06:32 +0530

    Update palindrome.js

--- 01-js/palindrome.js
+++ 01-js/palindrome.js
@@ -3,18 +3,15 @@
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
 */
 function isPalindrome(str) {
-  if (!str || typeof str !== 'string') {
-    return false; 
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
   }
-
-  const cleanedStr = str.toLowerCase();
-  const reversedStr = cleanedStr.split('').reverse().join('');
-
-  return cleanedStr === reversedStr;
+ 
+  return true; 
 }
-
-console.log(isPalindrome("Nan")); 
-console.log(isPalindrome("Hello")); 
-
 
 module.exports = isPalindrome;
