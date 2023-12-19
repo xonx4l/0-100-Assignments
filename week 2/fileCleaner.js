3eaed461723e4ca690fac2ce45c3bcb77906d424
xonx <119700621+xonx4l@users.noreply.github.com>
2023-12-19 23:13:45 +0530

    Update fileCleaner.js

--- week 2/fileCleaner.js
+++ week 2/fileCleaner.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
 
+const filePath = 'file.txt';
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const cleaned = data.replace(/\s+/g, ' ');
+  
+  fs.writeFile(filePath, cleaned, err => {
+    if (err) throw err;
+    
+    console.log('File updated with spaces removed');
+  });
+});
